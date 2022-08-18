e4dcbc7e300d332cdc9c23a30e9d0d0034ce12ac
Sarah Zeng <sayzeng@gmail.com>
2022-08-17 20:52:19 -0500

    added about, projects pages and navbar

--- project/start.js
+++ project/start.js
@@ -1,19 +1,27 @@
-var express = require('express');
-var app = express();
+var express = require('express'); // indicate that we want to use Express
+var app = express();  // create an Express app
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname)); // tell Express which directory to look in for files
 
 // views is directory for all template files
-app.set('views', __dirname + '/html');
-app.set('view engine', 'ejs');
+app.set('views', __dirname + '/html');  // tell Express that "html" folder holds app's template files
+app.set('view engine', 'ejs');  // tell Express EJS is templating language
 
-app.get('/', function(request, response) {
+app.get('/', function(request, response) {  // tell Express to render index page when it gets a request
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
+app.get('/about', function(request, response) {  // tell Express to render index page when it gets a request
+  response.render('pages/about');
+});
+
+app.get('/projects', function(request, response) {  // tell Express to render index page when it gets a request
+  response.render('pages/projects');
+});
+
+app.listen(app.get('port'), function() { // binds applications to port on your machine and sneds message to console
   console.log('Node app is running on port', app.get('port'));
 });
 
